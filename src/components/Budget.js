7f74ac76d0a70bd9a8a4afc10fcee95c43291498
Craig Lin <Craig.HL.Lin@ibm.com>
2023-04-27 17:55:59 +0800

    Fix bugs.

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,31 +1,26 @@
 
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { currency, budget, expenses, dispatch } = useContext(AppContext);
-    const [tmpBudget, setTmpBudget] = useState(budget);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
     const changeEvent = (value) => {
-        if(value > totalExpenses) {
-            setTmpBudget(0);
-            alert("The value cannot exceed amount spent £"+totalExpenses);
-            dispatch({
-                type: 'SET_BUDGET',
-                payload: value,
-            });
-        } else if(value > 20000) {
-
-        } else {
-            setTmpBudget(value);
+        if(value < totalExpenses) {
+          alert('You cannot reduce the budget value lower than the spending.');
+          return;
         }
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value,
+        });
     };
     return (
         <div className='alert alert-secondary'>
             <span>Budget: {currency}
-                <input type="number" value={tmpBudget} onChange={(event) => changeEvent(event.target.value)} />
+                <input type="number" value={budget} onChange={(event) => changeEvent(event.target.value)} />
             </span>
         </div>
     );
